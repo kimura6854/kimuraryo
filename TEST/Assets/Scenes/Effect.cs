943d1589ecc0e2e2f2e46875c597a3dca24c30c1
kimura_ryo <kimuraryo6854@gmail.com>
2020-03-27 16:46:30 +0900

    test

---
+++ TEST/Assets/Scenes/Effect.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect : MonoBehaviour
+{
+    
+    public Vector3 pos;
+    GameObject smorkobj;
+    GameObject fire;
+    GameObject[] prefub=new GameObject[2];
+    player playerscript;
+    GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prefub[0] = (GameObject)Resources.Load("smork");
+        prefub[1]= (GameObject)Resources.Load("fire");
+        //プレハブ生成
+        smorkobj =Instantiate(prefub[0], this.transform.position, Quaternion.identity);
+        obj = this.gameObject;
+       
+      
+            playerscript = obj.GetComponent<player>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            //if (playerscript.LIVE == false)
+            //{
+            //    Destroy(smorkobj);
+            //    playerscript.LIVE = true;
+            //}
+            //if (Input.GetKeyUp(KeyCode.Return))
+            //{
+            //    smorkobj = Instantiate(prefub[0], this.transform.position, Quaternion.identity);
+            //}
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            fire = Instantiate(prefub[1], this.transform.position, Quaternion.identity);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Destroy(fire);
+        }
+    }
+}
---
+++ TEST/Assets/Scenes/icebox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class icebox : MonoBehaviour
+{
+    GameObject obj;
+    public bool Hit=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        obj = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Hit)
+        {
+            Destroy(obj);
+        }   
+    }
+}
